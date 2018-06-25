e894404dc00fbf5c5ebe3d8a31ea3d06af6fd40f
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-06-25 13:42:57 -0400

    Template project

---
+++ Webpack_Template/src/index.js
@@ -0,0 +1,10 @@
+import * as d3 from "d3";
+import css from './style/main.css';
+import * as tst from './tst.js';
+
+d3.select("body")
+    .append("div")
+        .style("text-align","center")
+        .style("font-size","200px")
+        .text("Hello World");
+        
---
+++ Webpack_Template/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Hello World',
+        template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+}
