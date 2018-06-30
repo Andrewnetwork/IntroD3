2097c4e2d11de70cae2fd634fdbc9220dd65d516
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-06-30 00:19:31 -0400

    Basic MNIST Viewer

--- MNIST/src/index.js
+++ MNIST/src/index.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import css from "./style/main.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {plotImg,changeSize} from "./ui";
+import {plotImg,changeSize,plotImgs} from "./ui";
 
 export var MNIST = null;
 
@@ -12,12 +12,14 @@ d3.dsv(",", "data/train.csv", function(d) {
 })
 .then(function(data) { 
   MNIST = data;
+  d3.select("#loadingDiv").style("visibility","hidden");
   main();
+  d3.select("body").append("div").attr("id","imgDisp");
+  plotImgs("#imgDisp",0,100);
+  
 });
 
 function main(){
-  d3.select("#loadingDiv").style("visibility","hidden");
-
   plotImg(MNIST[0]);
 
   d3.select("#slideContainer")
@@ -35,7 +37,7 @@ function main(){
       .attr("type","range")
       .attr("min","1")
       .attr("max","16")
-      .attr("value","10")
+      .attr("value","4")
       .attr("class","slider")
       .on("input",(events,selector,input)=>changeSize(input[0].value));
 }
--- MNIST/src/ui.js
+++ MNIST/src/ui.js
@@ -3,12 +3,54 @@ import {MNIST} from "./index";
 
 const width = 28;
 const height = 28;
-var atomicUnit = 10;
+var atomicUnit = 4;
 
 export function changeSize(u){
     const idx = d3.select("#idx").property("value");
     atomicUnit = parseInt(u); 
     plotImg(MNIST[idx]);
+}
+
+export function plotImgs(uid,idx,timeOut){
+    if(idx<MNIST.length){
+        if(idx%10==0){
+            d3.select(uid).selectAll("*").remove();
+        }
+        plotImg2(MNIST[idx],uid);
+        setTimeout(()=>plotImgs(uid,idx+1,timeOut),timeOut);
+    }
+}
+
+export function plotImg2(Xy,uid){
+    const X = Xy[0]
+    const y = Xy[1];
+
+    var xCounter = 0;
+    var yCounter = 0;
+    var y_Counter = 0;
+
+    d3.select(uid)
+        .append("svg")
+            .attr("height",height*atomicUnit)
+            .attr("width", width*atomicUnit)
+            .selectAll("rect")
+            .data(X).enter()
+        .append("rect")
+            .attr("width",atomicUnit)
+            .attr("height",atomicUnit)
+            .style("fill","rgb(0,0,0)")
+            .attr("fill-opacity",(d)=>d/255)
+            .attr("x",(d)=>{
+                xCounter = (xCounter+atomicUnit)%(width*atomicUnit);
+                return xCounter;
+            })
+            .attr("y",(d)=>{
+                y_Counter += atomicUnit
+                if((y_Counter+atomicUnit) % (height*atomicUnit) == 0){
+                    yCounter += atomicUnit;
+                }
+                return yCounter;
+            });
 }
 
 export function plotImg(Xy){
@@ -30,6 +72,7 @@ export function plotImg(Xy){
             .append("svg")
                 .attr("height",height*atomicUnit)
                 .attr("width", width*atomicUnit)
+                .on("click",()=>alert(y))
                 .selectAll("rect")
                 .data(X).enter()
             .append("rect");
@@ -38,6 +81,7 @@ export function plotImg(Xy){
         d3.select("#disp").select("svg")
             .attr("height",height*atomicUnit)
             .attr("width", width*atomicUnit)
+            .on("click",()=>alert(y))
             .selectAll("rect");
             
         // The display container already has an svg canvas. 
